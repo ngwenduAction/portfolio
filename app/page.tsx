86eb9fa7c331135ae84d85d54f17fefe54418293
NJ Gambu <ngwendu.action@gmail.com>
2024-07-17 14:28:06 +0200

    Work Experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -19,6 +20,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
