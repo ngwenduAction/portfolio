ac99c8315daf70b0457d33ead496ae029887807d
NJ Gambu <gambungwendu@gmail.com>
2025-09-10 03:14:04 +0200

    updates

--- data/index.ts
+++ data/index.ts
@@ -44,7 +44,7 @@ export const navItems = [
     },
     {
       id: 4,
-      title: "Tech enthusiast with a passion for development.",
+      title: "Tech enthusiast with a passion for development",
       description: "",
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
